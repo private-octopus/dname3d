7650771e2c34e855d9b24ea1ec2ddf8a8a3f9017
Christian Huitema <huitema@huitema.net>
2021-06-21 16:32:28 -0700

    Add script to assess dga13

---
+++ stats/do_namestats.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# Extract name statistics from a set of files.
+# Since the number of files to merge can be quite large, we spread the processing
+# on all available cores.
+
+
+import sys
+import namestats
+import traceback
+import time
+import concurrent.futures
+import os
+
+def usage(argv_0):
+    print("Usage:\n" + argv_0 + " result_file temp name_file*")
+    print("    result_file:  file in which results will be collected.")
+    print("    temp:         prefix for temporary files (or \"-\" if only 1 process).")
+    print("    name_file*:   at least one file containing name lists.")
+
+class name_bucket:
+    def __init__(self, bucket_id, result_file_name, input_files):
+        self.bucket_id = bucket_id
+        self.input_files = input_files
+        self.result_file_name = result_file_name
+        self.stats = namestats.namestats()
+
+    def load(self):
+        try:
+            for input_file in self.input_files:
+                self.stats.load_logfile(input_file)
+        except:
+            traceback.print_exc()
+            print("Abandon bucket " + str(self.bucket_id))
+        return True
+
+    def save(self):
+        self.stats.export_result_file(self.result_file_name)
+
+def load_name_bucket(bucket):
+    bucket.load()
+    bucket.save()
+
+# main loop
+def main():
+    if len(sys.argv) < 4:
+        usage(sys.argv[0])
+        exit(1)
+    result_file = sys.argv[1]
+    temp_prefix = sys.argv[2]
+    nb_process = os.cpu_count()
+    files = sys.argv[3:len(sys.argv)]
+    nb_files = len(files)
+    bucket_list = []
+    print("Aiming for " + str(nb_process) + " processes")
+
+    # prepare a set of sublists and result names for each process.
+    process_left = nb_process
+    if temp_prefix == "-":
+        process_left = 1
+    files_left = nb_files
+    bucket_id = 0
+    while files_left > 0:
+        nb_file_this_process = int(files_left / process_left)
+        if nb_file_this_process == 0:
+            nb_file_this_process = 1
+        this_bucket_files = files[files_left - nb_file_this_process : files_left]
+        bucket_id += 1
+        temp_name = temp_prefix + str(bucket_id) + ".csv"
+        this_bucket = name_bucket(bucket_id, temp_name, this_bucket_files)
+        bucket_list.append(this_bucket)
+        files_left -= nb_file_this_process
+        process_left -= 1
+
+    nb_process = min(nb_process, len(bucket_list))
+    print("Will use " + str(nb_process) + " processes, " + str(len(bucket_list)) + " buckets")
+    
+    start_time = time.time()
+    if len(bucket_list) > 1:
+        with concurrent.futures.ProcessPoolExecutor(max_workers = nb_process) as executor:
+            future_to_bucket = {executor.submit(load_name_bucket, bucket):bucket for bucket in bucket_list }
+            for future in concurrent.futures.as_completed(future_to_bucket):
+                bucket = future_to_bucket[future]
+                try:
+                    data = future.result()
+                    sys.stdout.write(".")
+                    sys.stdout.flush()
+                except Exception as exc:
+                    traceback.print_exc()
+                    print('\nBucket %d generated an exception: %s' % (bucket.bucket_id, exc))
+        bucket_time = time.time()
+        print("\nThreads took " + str(bucket_time - start_time))
+        stats = namestats.namestats()
+        for bucket in bucket_list:
+            stats.import_result_file(bucket.result_file_name)
+            sys.stdout.write(".")
+            sys.stdout.flush()
+        summary_time = time.time()
+        print("\nSummary took " + str(summary_time - start_time))
+        stats.final_dga()
+        print("Removing " + str(len(stats.dga_sample)) + " dga samples. ")
+        stats.dga_sample = []
+        stats.export_result_file(result_file)
+    else:
+        bucket_list[0].result_file_name = result_file
+        bucket_list[0].load()
+        bucket_list[0].stats.final_dga()
+        bucket_list[0].save()
+        print("Loaded a single bucket into " + result_file)
+
+# actual main program, can be called by threads, etc.
+
+if __name__ == '__main__':
+    main()
---
+++ stats/namestats.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# Parse the names file and extract statistics similar to sum3,
+# but also including the dga13 category.
+
+import traceback
+import nameparse
+import gzip
+import traceback
+
+def add_to_list(table, name, count):
+    if name in table:
+        table[name] += count
+    else:
+        table[name] = count
+
+class namestats:
+    def __init__(self):
+        #self.list = dict()
+        self.sum_by_cat = dict()
+        self.maybe_dga = dict()
+        self.dga_tld = dict()
+        self.dga_sample = []
+
+    def final_dga(self):
+        for name in self.maybe_dga:
+            count = self.maybe_dga[name]
+            if count != 1:
+                add_to_list(self.sum_by_cat, "tld", count)
+                # add_to_list(self.list, name, count)
+            else:
+                add_to_list(self.sum_by_cat, "dga13", count)
+                parts = name.split(".")
+                if len(parts) == 2:
+                    add_to_list(self.dga_tld, parts[1], count)
+                    if len(self.dga_sample) < 1000:
+                        self.dga_sample.append(name)
+        self.dga_tld = dict()
+
+    def load_name(self, name, count):
+        parts = name.split(".")
+        nb_parts = len(parts)
+        # remove final dot if any
+        while nb_parts > 0 and len(parts[nb_parts-1]) == 0:
+            nb_parts -= 1
+        # TODO: trim to specified depth
+        if nb_parts > 0 and parts[nb_parts -1] == "ARPA":
+            add_to_list(self.sum_by_cat, "arpa", count)
+        elif nb_parts == 2 and (len(parts[0]) == 12 or len(parts[0]) == 13):
+            add_to_list(self.maybe_dga, name, count)
+        else:
+            add_to_list(self.sum_by_cat, "tld", count)
+
+    def load_logline(self, line):
+        nl = nameparse.nameline()
+        if nl.from_csv(line):
+            if nl.name_type == "tld":
+                self.load_name(nl.name, nl.count)
+            else:
+                add_to_list(self.sum_by_cat, nl.name_type, nl.count)
+
+    def load_logfile_csv(self, logfile):
+        for line in open(logfile , "rt"):
+            self.load_logline(line)
+
+    def load_logfile_gz(self, logfile):
+        try:
+            with gzip.open(logfile,'rt') as fin: 
+                for line in fin:
+                    self.load_logline(line)
+        except Exception as e:
+            traceback.print_exc()
+            print("Cannot process compressed file <" + logfile  + ">\nException: " + str(e))
+            print("Giving up");
+            exit(1) 
+
+    def load_logfile(self, logfile):
+        if logfile.endswith(".gz"):
+            self.load_logfile_gz(logfile)
+        else:
+            self.load_logfile_csv(logfile)
+
+    def export_result_file(self, result_file):
+        #self.list = dict()
+        f = open(result_file , "wt", encoding="utf-8")
+        for cat in self.sum_by_cat:
+            f.write("catsum," + cat + "," + str(self.sum_by_cat[cat]) + "\n")
+        for dga in self.maybe_dga:
+            f.write("maybe_dga," + dga + "," + str(self.maybe_dga[dga]) + "\n")
+        for tld in self.dga_tld:
+            f.write("dga_tld," + tld + "," + str(self.dga_tld[tld]) + "\n")
+        for dga in self.dga_sample:
+            f.write("dga_sample," + dga + ",1\n")
+        f.close()
+
+    def import_result_file(self, result_file):
+        #self.list = dict()
+        for line in open(result_file , "rt", encoding="utf-8"):
+            parts = line.split(",")
+            if len(parts) != 3:
+                print("Unexpected line in " + result_file + "\n" + line + "\ngiving up")
+                exit(1)
+            try:
+                count = int(parts[2])
+            except:
+                print("Unexpected count in " + result_file + "\n" + line + "\ngiving up")
+                exit(1)
+            if parts[0] == "catsum":
+                add_to_list(self.sum_by_cat, parts[1], count)
+            elif parts[0] == "maybe_dga":
+                add_to_list(self.maybe_dga, parts[1], count)
+            elif parts[0] == "dga_tld":
+                add_to_list(self.maybe_dga, parts[1], count)
+            elif parts[0] == "dga_sample":
+                self.dga_sample.append(parts[1])
+            else:
+                print("Unexpected table in " + result_file + "\n" + line + "\ngiving up")
+                exit(1)
