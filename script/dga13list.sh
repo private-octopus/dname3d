95258d0fda7b5a60a4aff5511774eb2cfa10b4a8
Christian Huitema <huitema@huitema.net>
2021-07-18 12:19:09 -0700

    Add script for dga13 list extraction

---
+++ script/dga13list.sh
@@ -0,0 +1,12 @@
+instance=$1
+date=$2
+eastwest=$3
+
+mkdir -p ~/$instance
+
+ls -d /data/ITHI/results-name/$eastwest/results-$instance/$date* >~/$instance/list-$date
+
+rm ~/$instance/tmp-*
+
+python3 dga13_extract_names.py ~/$instance/dga13_sfx-$date.csv  ~/$instance/suffixes-$date.csv ~/$instance/tmp- ../data/dga13_subnets.txt `cat ~/$instance/list-$date`
+
