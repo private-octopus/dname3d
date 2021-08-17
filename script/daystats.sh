cdbd3ef18b1d2748029538ea200922ce71b80e59
Christian Huitema <huitema@huitema.net>
2021-08-17 13:10:23 -0700

    no attempt if no file

--- script/daystats.sh
+++ script/daystats.sh
@@ -6,7 +6,11 @@ mkdir -p ~/$instance
 
 ls -d /data/ITHI/results-name/$eastwest/results-$instance/$date* >~/$instance/list-$date
 
-rm ~/$instance/tmp-*
+rm -f ~/$instance/tmp-*
 
-python3 do_namestats.py ~/$instance/stats-$date.csv  ~/$instance/suffixes-$date.csv ~/$instance/tmp- ../data/dga13_subnets.txt `cat ~/$instance/list-$date`
+if [ -s ~/$instance/list-$date ]; then
+    python3 do_namestats.py ~/$instance/stats-$date.csv  ~/$instance/suffixes-$date.csv ~/$instance/tmp- ../data/dga13_subnets.txt `cat ~/$instance/list-$date`
+else
+    echo "empty file: ~/$instance/list-$date"
+fi
 
