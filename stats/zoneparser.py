bc21c62f422b4ba9a35c6b7b9c4df2855f9dd861
Christian Huitema <huitema@huitema.net>
2021-09-20 11:39:25 -0700

    Add debug information

--- stats/zoneparser.py
+++ stats/zoneparser.py
@@ -38,16 +38,21 @@ class lru_list:
             if key in self.table:
                 # bring to top
                 if not key == self.lru_first:
-                    if key == self.lru_last:
-                        self.lru_last = self.table[key].lru_previous
-                        self.table[self.lru_last].lru_next = ""
-                    else:
-                        self.table[self.table[key].lru_next].lru_previous = self.table[key].lru_previous
-                        self.table[self.table[key].lru_previous].lru_next = self.table[key].lru_next
-                    self.table[key].lru_previous = ""
-                    self.table[key].lru_next = self.lru_first
-                    self.table[self.lru_first].lru_previous = key
-                    self.lru_first = key
+                    try:
+                        if key == self.lru_last:
+                            self.lru_last = self.table[key].lru_previous
+                            self.table[self.lru_last].lru_next = ""
+                        else:
+                            self.table[self.table[key].lru_next].lru_previous = self.table[key].lru_previous
+                            self.table[self.table[key].lru_previous].lru_next = self.table[key].lru_next
+                        self.table[key].lru_previous = ""
+                        self.table[key].lru_next = self.lru_first
+                        self.table[self.lru_first].lru_previous = key
+                        self.lru_first = key
+                    except:
+                        traceback.print_exc()
+                        print("Could not promote <" + key + "> (" + self.table[key].lru_previous + "," + self.table[key].lru_next + ") after " + str(len(self.table)) + " (" + self.lru_first + "," + self.lru_last + ")")
+                        ret = False
             else:
                 # add an entry to the list
                 self.table[key] = lru_list_entry(self.target_class)
