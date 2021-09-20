4eaffa7650259598891d838faee676d76738cbbb
Christian Huitema <huitema@huitema.net>
2021-09-20 16:30:49 -0700

    Add fuzz test of LRU logic.

--- stats/lru_test.py
+++ stats/lru_test.py
@@ -1,13 +1,14 @@
-# Unit test of the zoneparser module
+# Unit test of the LRU module
 #
-# Expect these tests to work:
+# Expect this test to work:
 #
-# ...
+# py .\lru_test.py 5 ..\data\lru_test_in.txt ..\data\lru_test_ref.csv ..\tmp\lru_test_res.csv
 
 import sys
 import zoneparser
 import compare_file_test
 import traceback
+import random
 
 class lru_test_entry:
     def __init__(self):
@@ -33,7 +34,8 @@ def lru_list(lru):
 
 limit = int(sys.argv[1])
 test_in = sys.argv[2]
-test_out = sys.argv[3]
+test_ref = sys.argv[3]
+test_out = sys.argv[4]
 
 lru = zoneparser.lru_list(limit,lru_test_entry)
 print("LRU list max " + str(lru.target_number) + ", starts with " + str(len(lru.table)))
@@ -42,7 +44,7 @@ for line in open(test_in, "rt"):
     try:
         rank += 1
         key = line.strip()
-        if lru.add(line.strip()):
+        if lru.add(key):
             lru.table[key].data.hit_count += 1
             lru.table[key].data.last_hit = rank
             if lru.table[key].data.first_hit < 0:
@@ -53,13 +55,28 @@ for line in open(test_in, "rt"):
     except:
         traceback.print_exc()
         print("Could not load " + test_in + " line " + str(rank) + ": " + key)
-        break
+        exit(1)
         
 f = open(test_out,"wt")
 f.write("Key,hit-count,first_rank,last_rank\n")
+nb_saved = 0
 key = lru.lru_first
 while key != "":
     f.write(key + "," + str(lru.table[key].data.hit_count) + "," +  str(lru.table[key].data.first_hit) + "," +  str(lru.table[key].data.last_hit) + "\n")
     key = lru.table[key].lru_next
+    nb_saved += 1
 f.close()
+print("saved " + str(nb_saved) + " entries.")
+# Compare outouts to expected result
+if not compare_file_test.compare_files(test_out, test_ref):
+    exit(1)
 
+fuzz_list = []
+for line in open(test_in, "rt"):
+    fuzz_list.append(line.strip())
+for x in range(0,10000000):
+    key = random.choice(fuzz_list)
+    if not lru.add(key):
+        print("Fuzz break " + str(x) + ": " + key)
+        exit(1)
+print("Fuzz complete.")
