9c63eebbc469016750c5eef052ff2bce06ddc0f5
huitema <huitema@huitema.net>
2021-12-09 01:52:16 +0000

    Adding the central million script.

---
+++ script/central_million.sh
@@ -0,0 +1,13 @@
+# /bin/bash!
+#
+# Start collection of result file collecting DNS lookup results
+# from the million list
+
+ip2as=../data/ip2as.csv
+pub_s=../data/public_suffix_list.dat
+mill=../data/million.txt
+result=/home/huitema/dns_millions/dns_millions_$1.csv
+temp=/home/huitema/tmp/dnslookup_
+nb=$2
+python3 do_dnslookup.py $nb $ip2as $pub_s $mill $result $temp
+
