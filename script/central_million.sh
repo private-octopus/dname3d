2a7ae52fb03ec1039f1e32a5cc10133d9e9134e3
Christian Huitema <huitema@huitema.net>
2021-12-09 21:53:51 -0800

    Remove temp files before running script

--- script/central_million.sh
+++ script/central_million.sh
@@ -9,5 +9,6 @@ mill=../data/million.txt
 result=/home/huitema/dns_millions/dns_millions_$1.csv
 temp=/home/huitema/tmp/dnslookup_
 nb=$2
+rm $temp*
 python3 do_dnslookup.py $nb $ip2as $pub_s $mill $result $temp
 
