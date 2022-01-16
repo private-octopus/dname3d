6c75ce36927b303cb11646557f340cc2d8609c15
Christian Huitema <huitema@huitema.net>
2022-01-15 17:36:55 -1000

    Produce metric M9

--- stats/dnslookup_stats.py
+++ stats/dnslookup_stats.py
@@ -1,10 +1,32 @@
 #!/usr/bin/python
 # coding=utf-8
 #
-# This script collects data on sites listed in a master file such as the "magnificent million".
-# The strategy is to pick at random names of small and large files, get the data, and add it
-# to the result file. Running the program in the background will eventually accumulate enough
-# data to do meaning ful statistics.
+# This script extracts the metric M9 from the statistics collected on the
+# selected million names, and from the statistics on the COM zone.
+# 
+# The M9 metrict has the following data:
+#
+# M9.1.* -- statistics for top 100 hosts
+# M9.2.* -- statistics for next 900 hosts
+# M9.3.* -- statistics for next 9000 hosts
+# M9.4.* -- statistics for next 90000 hosts
+# M9.5.* -- statistics for next 900000 hosts
+# M9.5.* -- statistics for .COM hosts
+#
+# For each class, we measure:
+#
+# M9.*.1, service name -- share of names served by a given server with at least 1% market share
+# M9.*.2 -- number of services for 50% of names
+# M9.*.3 -- number of services for 90% of names
+# M9.*.4 -- average number of services per name
+#
+# Example:
+# M9.2.1,2017-01-31,v2.0, cloudflare.com , 0.209763
+# M9.2.1,2017-01-31,v2.0, AWS , 0.11023
+# M9.2.1,2017-01-31,v2.0, etc. , 0.05000
+# M9.2.2,2017-01-31,v2.0, , 9
+# M9.2.3,2017-01-31,v2.0, , 120
+# M9.2.4,2017-01-31,v2.0, , 1.2
 
 import sys
 import dns.resolver
@@ -26,10 +48,14 @@ def load_dns_file(dns_json):
     loaded = 0
     for line in open(dns_json, "rt"):
         dns_look = dnslook.dnslook()
-        dns_look.from_json(line)
-        stats.append(dns_look)
-        loaded += 1
-        if loaded%100 == 0:
+        try:
+            dns_look.from_json(line)
+            stats.append(dns_look)
+            loaded += 1
+        except Exception as e:
+            traceback.print_exc()
+            print("Cannot parse <" + line  + ">\nException: " + str(e))
+        if loaded%500 == 0:
             sys.stdout.write(".")
             sys.stdout.flush()
     return stats
@@ -87,12 +113,23 @@ class stats_one:
     def __init__(self):
         self.p50 = 0
         self.p90 = 0
+        self.nb_names = 0
         self.total = 0
+        self.service_list = dict()
         self.full_list = []
 
     def add(self, name, count):
         self.total += count
-        self.full_list.append(stats_one_entry(name, count))
+        if name in self.service_list:
+            list_index = self.service_list[name]
+            self.full_list[list_index].count += count
+        else:
+            list_index = len(self.full_list)
+            self.service_list[name] = list_index
+            self.full_list.append(stats_one_entry(name, count))
+
+    def add_name_count(self, nb_names):
+        self.nb_names += nb_names
 
     def compute(self):
         self.full_list.sort(key=functools.cmp_to_key(compare_stats_entry), reverse=True)
@@ -113,6 +150,19 @@ class stats_one:
         for i in range(0,5):
             print(name + "["+ str(i) + "]: " + self.full_list[i].name + ", " + str(self.full_list[i].count/self.total))
 
+    def m9(self, rank, m9date, F):
+        metric = "M9." + str(rank+1); 
+        if self.nb_names > 0:
+            F.write(metric + ".1," + m9date +",v2.0, ," + str(self.total/self.nb_names) + "\n")
+        F.write(metric + ".2," + m9date + ",v2.0, ," + str(self.p50) + "\n")
+        F.write(metric + ".3," + m9date + ",v2.0, ," + str(self.p90) + "\n")
+        for i in range(0,20):
+            fraction = self.full_list[i].count/self.total
+            if i > 4 and fraction < 0.005:
+                break
+            F.write(metric + ".4," + m9date + ",v2.0, " + self.full_list[i].name + "," + str(fraction) + "\n")
+           
+
 
 # Main
 
@@ -121,6 +171,10 @@ public_suffix_file = sys.argv[3]
 dups_file = sys.argv[4]
 zone_result_file = sys.argv[5]
 result_file = sys.argv[6]
+m9_date = sys.argv[7]
+
+
+print("Set to compute metric M9 in " + result_file + " for " + m9_date)
 
 ps = pubsuffix.public_suffix()
 if not ps.load_file(public_suffix_file):
@@ -149,18 +203,33 @@ print("test ranks:" + rank_string)
 stats = load_dns_file(sys.argv[1])
 print("\nLoaded " + str(len(stats)) + " lines.")
 
-ns_dict = dict()
+# Process the statistics acquired from the million list.
+# Compute a list of per service statistics (ns_this_name),
+# each containing a vector of 6 numbers: 0..5 are the sum
+# of name of each category from the million file, [5] is
+# the sum of names for this service in the com file.
 
+# First prepare an empty table of 6 categories.
+level_name = ["top 100", "next 900", "next 9000", "next 90000", "next 900000", "COM" ]
+level_stats = []
+for i in range(0,6):
+    level_stats.append(stats_one())
+
+# Fill the category index 5 with results from the com zone
+for service in zp.sf_dict:
+    level_stats[5].add(service, zp.sf_dict[service].name_count)
+level_stats[5].add_name_count(zp.name_count)
+
+# Fill categories indices 0 to 4 with results from the million look ups
 nb_fail = 0
-total_rank = rank_count();
 for dns_lookup in stats:
-    # check that the names used in the domain property are consistent with the names in the million list.
-    # may want to use suffixes in the million list!
+    # use suffixes in the million list!
     ns_this_name = dict()
+    if dns_lookup.domain == "":
+        continue
     m_class = get_million_class(dns_lookup.domain, zp.millions)
-    total_rank.count[m_class] += 1
     if m_class > 4:
-        print("could not find " + dns_lookup.domain + " in millions.")
+        print("could not find <" + dns_lookup.domain + "> in millions.")
         nb_fail += 1
         if nb_fail > 10:
             break
@@ -171,40 +240,17 @@ for dns_lookup in stats:
         else:
             ns_this_name[ns_suffix] = 1
     for ns_suffix in ns_this_name:
-        if not ns_suffix in ns_dict:
-            ns_dict[ns_suffix] = rank_count()
-        ns_dict[ns_suffix].count[m_class] += 1
-print("processed the names")
+        level_stats[m_class].add(ns_suffix, 1)
+    level_stats[m_class].add_name_count(1)
 
+# Compute the statistics for each category
+for m_class in range(0,6):
+    level_stats[m_class].compute()
+    level_stats[m_class].comment(level_name[m_class])
 
-with open(result_file,"wt") as f:
-    f.write("name, 100, 1K, 10K, 100K, 1M, all\n");
-    f.write("count");
-    for count in total_rank.count:
-        f.write("," + str(count));
-    f.write("\n")
-    for ns_name in ns_dict:
-        f.write(ns_name)
-        m_class = ns_dict[ns_name]
-        for m_count in m_class.count:
-            f.write(","+str(m_count))
-        f.write("\n")
-        
-print("saved " + str(len(ns_dict)) + " services.")
+# publish the metric file
+print("saving metric M9 in " + result_file + " for " + m9_date)
+with open(result_file,"wt") as F:
+    for m_class in range(0,6):
+        level_stats[m_class].m9(m_class, m9_date, F)
 
-level_stats = []
-level_name = "top_10"
-for i in range(0,5):
-    level_name += "0"
-    level_stats.append(stats_one())
-    for name in ns_dict:
-        if ns_dict[name].count[i] > 0:
-            level_stats[i].add(name, ns_dict[name].count[i])
-    level_stats[i].compute()
-    level_stats[i].comment(level_name)
-
-zone_stats = stats_one()
-for service in zp.sf_dict:
-    zone_stats.add(service, zp.sf_dict[service].hit_count)
-zone_stats.compute()
-zone_stats.comment("Com")
