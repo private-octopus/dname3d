fd84fd872d2f6ab7095a8fa7efa29fcff7fe451e
Christian Huitema <huitema@huitema.net>
2023-03-03 15:25:04 -0800

    Update M9 compute script.

--- script/m9plus_cron.sh
+++ script/m9plus_cron.sh
@@ -21,22 +21,71 @@ else
     python3 getmajestic.py $MILLION
 fi
 
-COM_STAT="/home/huitema/com_stats/com_stats_$YEAR$MM.csv"
-if [ -f $COM_STAT ];
+#
+# TODO locate com zone required for parsing or sampling
+#
+
+COM_SAMPLES="/home/huitema/com_stats/com_samples_$YYYYMM.csv"
+COM_STATS="/home/huitema/com_stats/com_stats_$YYYYMM.csv"
+PUB_S="../data/public_suffix_list.dat"
+DUP_S="../data/service-duplicates.csv"
+X=""
+for i in `ls /data/ZFA-backups/$YYYYMM*/com/com.zone`; do
+    if [[ "$i" > "$X" ]]; then
+        X="$i"; 
+    fi; 
+done
+if [ -f COM_STATS ];
 then
-    echo "$COM_STAT already created";
+    echo "COM_STATS already computed";
 else
-    echo "Need to compute $COM_STAT"
-    ../script/central_dns.sh $YYYYMM
+    if [ ! -z "$X" ]; then
+        S_TEMP=/home/huitema/tmp_com_zone_
+        rm $S_TEMP*
+        python3 do_zoneparser.py $COM_STATS $X $PUB_S $DUP_S %MILLION $S_TEMP
+        rm $S_TEMP*
+    fi
 fi
-echo "Adding $NBSAMPLES to the million list"
-../script/central_million.sh $YYYYMM $NBSAMPLES
-echo "Recomputing M9 for $YEAR-$MM-$DAY"
-../script/compute_m9.sh $YEAR $MM $DAY
-echo "Writing M9 to ITHI staging server"
-cd /home/huitema/
-rsync -Cav -e "ssh -l octo0" M9 octo0@ithi.research.icann.org:data
-# scp /home/huitema/M9/M9-$YEAR-$MM-$DAY.csv octo0@ithi.research.icann.org:data/M9/
-echo "M9 updated."
+
+# TODO: use com samples
+if [ -f $COM_SAMPLES ];
+then
+    echo "$COM_SAMPLES already computed";
+else
+    if [ ! -z "$X" ]; then
+        echo "Found COM zone file: $X"
+        Z_TEMP=/home/huitema/tmp/tmp_com_sample_
+        rm $Z_TEMP*
+        python3 do_zonesampler.py $COM_SAMPLES $X 1000000 $Z_TEMP
+    fi
+fi
+
+RESULT="/home/huitema/dns_millions/dns_millions_$YYYYMM.csv"
+if [ -f $COM_SAMPLES -a -f $MILLION ];
+then
+echo "Adding $NBSAMPLES to the DNS processed list"
+    #../script/central_million.sh $YYYYMM $NBSAMPLES
+    # TODO: IP2AS should be dynamic, monthly download.
+    ip2as=../data/ip2as.csv
+    TEMP=/home/huitema/tmp/dnslookup_
+    rm $TEMP*
+    python3 do_dnslookup.py $NBSAMPLES $COM_SAMPLES $ip2as $PUB_S $MILLION $RESULT $TEMP
+fi
+
+if [ -f $RESULT ]
+then
+    echo "Recomputing M9 for $YEAR-$MM-$DAY"
+    ../script/compute_m9.sh $YEAR $MM $DAY
+    m9_day="$YEAR-$MM-$DAY"
+    m9_file="/home/huitema/M9/M9-$m9_day.csv"
+    echo "Computing M9 in $m9_file from $com_stats and $mill_stats"
+    python3 ./dnslookup_stats.py $RESULT $MILLION $PUB_S $DUP_S $COM_STATS $m9_file $m9_day
+    echo "Writing M9 to ITHI staging server"
+    cd /home/huitema/
+    rsync -Cav -e "ssh -l octo0" M9 octo0@ithi.research.icann.org:data
+    # scp /home/huitema/M9/M9-$YEAR-$MM-$DAY.csv octo0@ithi.research.icann.org:data/M9/
+    echo "M9 updated."
+fi
+
 cd $OLD_DIR
 pwd
