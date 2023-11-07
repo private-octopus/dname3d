16a95fde84fd6398ff5f6168ed14e181b1535131
Christian Huitema <huitema@huitema.net>
2023-11-06 22:14:39 -0800

    Trial of IP address list

--- stats/find_ns_servers.py
+++ stats/find_ns_servers.py
@@ -230,19 +230,28 @@ def main():
     suffix_weights = key_weights()
     as_weights = key_weights()
     ns_as_weight2 = key_weights()
+    ns_ip_weight = key_weights()
     for dns_item in millions:
         ns_suffixes = set()
         as_numbers = set()
         ns_as_numbers = set()
+        ns_ip_addresses = set()
         for ns in dns_item.ns:
             nd.add_ns_name(ns)
             ns_suffix = zoneparser.extract_server_suffix(ns, ps, zp.dups)
             ns_suffixes.add(ns_suffix)
             for asn in nd.d[ns].ases:
                 ns_as_numbers.add(asn_ag.get_asn(asn))
+            for ipaddr in nd.d[ns].ip:
+                ns_ip_addresses.add(ipaddr)
+            for ipaddr6 in nd.d[ns].ipv6:
+                ns_ip_addresses.add(ipaddr6)
         ns_weights.add_names(dns_item.ns, dns_item.million_range)
         suffix_weights.add_names(ns_suffixes, dns_item.million_range)
         ns_as_weight2.add_names(ns_as_numbers, dns_item.million_range)
+        print("Adding " + str(len(ns_ip_addresses)) + " addresses.")
+        ns_ip_weight.add_names(ns_ip_addresses, dns_item.million_range)
+        
         for asn in dns_item.ases:
             as_numbers.add(asn_ag.get_asn(asn))
         as_weights.add_names(as_numbers, dns_item.million_range)
@@ -298,6 +307,14 @@ def main():
     for kri in top_ns_as2:
         nb_top += 1
         print(str(nb_top) + ": " + str(kri.key)  + ", " + asns.name(kri.key) + ", " + str(kri.weight))
+        if nb_top >= 10:
+            break
+    top_ns_ip = ns_ip_weight.get_sorted_list(-1)
+    print("Top_ns_ip has : " + str(len(top_ns_ip)))
+    nb_top = 0
+    for kri in top_ns_ip:
+        nb_top += 1
+        print(str(nb_top) + ": " + str(kri.key)  + ", " + str(kri.key) + ", " + str(kri.weight))
         if nb_top >= 10:
             break
 
